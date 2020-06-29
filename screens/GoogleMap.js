41ed90a73af295792e95708934922cfb25b5a330
Gayan Jayakody <gayanjayakody13@gmail.com>
2020-06-29 18:27:13 +0530

    Create GoogleMap.js

---
+++ screens/GoogleMap.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import {SafeAreaView,StyleSheet,ScrollView,View,Text,StatusBar,Dimensions} from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+
+
+let { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 6.9271;
+const LONGITUDE = 79.8612;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+
+
+class Map extends Component {
+	
+  constructor() {
+    super();
+    this.state = {
+	  lat:LATITUDE,
+	  lng:LONGITUDE
+    };
+  }
+ 
+  render() {
+    return (
+		<View style={styles.container}>
+     		<MapView
+		       	provider={PROVIDER_GOOGLE}
+		       	style={styles.map}
+          		showsUserLocation={true}
+				followsUserLocation={true}
+				showsMyLocationButton={true}
+          		zoomEnabled={true}  
+          		zoomControlEnabled={true}
+		       	region={{
+        			latitude: this.state.lat,
+        			longitude: this.state.lng ,
+        			latitudeDelta: 0.0122,
+        			longitudeDelta: 0.022,
+      			}}
+		     >
+				<Marker  
+            		coordinate={{latitude: this.state.lat ,
+            		longitude: this.state.lng}}   
+          		/>  
+		    </MapView>
+			<GooglePlacesAutocomplete
+			styles={{  description: {fontWeight: 'bold',backgroundColor:'white'},}}
+			autoFillOnNotFound={true}
+      		placeholder='Search'
+			fetchDetails = {true}
+      		onPress={(data, details = geometry.location) => {
+				this.setState({lat:details['geometry']['location']['lat'],lng:details['geometry']['location']['lng']})
+        		console.log(this.state.lat);
+				console.log(this.state.lng);
+      		}}
+      		query={{
+        		key: 'AIzaSyAmWH-rpAp3u2t6GSPB0cKg-aCsyj91NrQ',
+        		language: 'en',
+     	 	}}
+   	 	    />
+		</View>		
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+	container:{
+		...StyleSheet.absoluteFillObject
+	},
+	map:{
+		...StyleSheet.absoluteFillObject
+	}
+})
+	
+export default Map
+	
+	
+	
+	
